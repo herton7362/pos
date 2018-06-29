e3d50ad59bda193e206cf37b513f58bb1b06e3f7
herton7362 <he.tang@everydayratings.com>
2018-06-29 16:43:43 +0800

    商城版

--- src/main/resources/static/js/modules/activeRule/list.js
+++ src/main/resources/static/js/modules/activeRule/list.js
@@ -11,12 +11,14 @@ require(['jquery', 'vue', 'messager', 'utils'], function($, Vue, messager, utils
                     {field:'conditionValue', title:'激活奖励金额', formatter: function(value) {
                             return '大于 '+value;
                         }},
+                    {field:'awardMoney', title:'激活奖励金额'},
                     {field:'remark', title:'备注'}
                 ]
             },
             formData: {
                 id: null,
                 conditionValue: null,
+                awardMoney: null,
                 remark: null
             }
         },
