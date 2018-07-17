3fdc4e6263ae205745c45baba533d1e03d40f2d8
herton7362 <he.tang@everydayratings.com>
2018-07-17 09:08:51 +0800

    商城版

--- src/main/resources/static/js/modules/information/list.js
+++ src/main/resources/static/js/modules/information/list.js
@@ -9,6 +9,7 @@ require(['jquery', 'vue', 'messager', 'utils'], function($, Vue, messager, utils
                 },
                 columns: [
                     {field:'title', title:'标题', width: '300px'},
+                    {field:'category', title:'类别', width: '100px'},
                     {field:'content', title:'内容', formatter: function(value) {
                         if(value.length > 50) {
                             return '<span title="'+value+'">' + value.substring(0, 40) + '...</span>';
@@ -21,6 +22,7 @@ require(['jquery', 'vue', 'messager', 'utils'], function($, Vue, messager, utils
             formData: {
                 id: null,
                 title: null,
+                category: null,
                 content: null
             }
         },
