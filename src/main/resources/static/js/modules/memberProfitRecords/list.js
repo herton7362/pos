24d0b017d4fc1fceba552704fde516eb7dc29f95
herton7362 <he.tang@everydayratings.com>
2018-07-20 09:04:39 +0800

    商城版

--- src/main/resources/static/js/modules/memberProfitRecords/list.js
+++ src/main/resources/static/js/modules/memberProfitRecords/list.js
@@ -21,7 +21,17 @@ require(['jquery', 'vue', 'messager', 'utils'], function($, Vue, messager, utils
                     {field:'userName', title:'用户名称'},
                     {field:'transactionAmount', title:'交易金额'},
                     {field:'sn', title:'机器码'},
-                    {field:'transactionType', title:'交易类型'},
+                    {field:'transactionType', title:'交易类型', formatter: function (value) {
+                            if(1 == value) {
+                                return '返现奖';
+                            } else if(2 == value) {
+                                return '直营奖';
+                            } else if(3 == value) {
+                                return '管理奖';
+                            } else if(4 == value) {
+                                return '团建奖';
+                            }
+                        }},
                     {field:'profit', title:'收益'},
                     {field:'transactionDate', title:'交易日期', formatter: function (value) {
                             return new Date(value).format('yyyy-MM-dd HH:mm')
