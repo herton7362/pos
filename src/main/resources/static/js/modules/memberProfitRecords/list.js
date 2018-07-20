7c9b386012adb727681058f8fc9e4b5fbe2eddbb
herton7362 <he.tang@everydayratings.com>
2018-07-20 09:23:09 +0800

    商城版

--- src/main/resources/static/js/modules/memberProfitRecords/list.js
+++ src/main/resources/static/js/modules/memberProfitRecords/list.js
@@ -21,33 +21,48 @@ require(['jquery', 'vue', 'messager', 'utils'], function($, Vue, messager, utils
                     {field:'userName', title:'用户名称'},
                     {field:'transactionAmount', title:'交易金额'},
                     {field:'sn', title:'机器码'},
-                    {field:'transactionType', title:'交易类型', formatter: function (value) {
-                            if(1 == value) {
-                                return '返现奖';
-                            } else if(2 == value) {
-                                return '直营奖';
-                            } else if(3 == value) {
-                                return '管理奖';
-                            } else if(4 == value) {
-                                return '团建奖';
-                            }
-                            return '未知' + value;
-                        }},
+                    {field:'transactionTypeName', title:'交易类型'},
                     {field:'profit', title:'收益'},
                     {field:'transactionDate', title:'交易日期', formatter: function (value) {
                             return new Date(value).format('yyyy-MM-dd HH:mm')
                         }},
                     {field:'operateTransactionId', title:'批次号'}
                 ]
-            }
+            },
+            tradeTypes: {}
         },
         methods: {
             search: function () {
                 this.crudgrid.$instance.load(this.crudgrid.queryParams);
+            },
+            loadDictionary: function (fn) {
+                var self = this;
+                $.ajax({
+                    url: utils.patchUrl('/api/dictionary/code/TradeType'),
+                    cache: false,
+                    success: function (data) {
+                        $.each(data, function () {
+                            self.tradeTypes[this.code] = this.name;
+                        });
+                        fn();
+                    }
+                })
+            },
+            tableTransformResponse: function (data) {
+                var self = this;
+                $.each(data, function () {
+                    if(this.transactionType) {
+                        this.transactionTypeName = self.tradeTypes[this.transactionType + ''];
+                    }
+                });
+                return data;
             }
         },
         mounted: function() {
-           this.search();
+            var self = this;
+            this.loadDictionary(function () {
+                self.search();
+            });
         }
     });
 });
