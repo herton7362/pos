84a2c552a7a3776a8449925ff8038f2790cdebae
zhangqinghe123 <qingheemail@126.com>
2018-08-20 21:21:53 +0800

    修改导出列宽度

--- src/main/java/com/framework/module/member/web/MemberProfitExportController.java
+++ src/main/java/com/framework/module/member/web/MemberProfitExportController.java
@@ -96,8 +96,15 @@ public class MemberProfitExportController extends AbstractCrudController<MemberP
     private void createTitle(HSSFWorkbook workbook, HSSFSheet sheet) {
         HSSFRow row = sheet.createRow(0);
         //设置列宽，setColumnWidth的第二个参数要乘以256，这个参数的单位是1/256个字符宽度
-        sheet.setColumnWidth(1, 12 * 256);
-        sheet.setColumnWidth(3, 17 * 256);
+        sheet.setColumnWidth(1, 20 * 256);
+        sheet.setColumnWidth(2, 20 * 256);
+        sheet.setColumnWidth(3, 20 * 256);
+        sheet.setColumnWidth(4, 20 * 256);
+        sheet.setColumnWidth(5, 20 * 256);
+        sheet.setColumnWidth(6, 20 * 256);
+        sheet.setColumnWidth(7, 20 * 256);
+        sheet.setColumnWidth(8, 20 * 256);
+        sheet.setColumnWidth(9, 25 * 256);
 
         //设置为居中加粗
         HSSFCellStyle style = workbook.createCellStyle();
