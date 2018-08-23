09b2c24dc51cf44d033724780940dd98177f83d2
herton7362 <he.tang@everydayratings.com>
2018-08-23 12:30:43 +0800

    商城版

--- src/main/java/com/kratos/config/OAuth2ServerConfig.java
+++ src/main/java/com/kratos/config/OAuth2ServerConfig.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -30,6 +31,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
@@ -93,7 +96,20 @@ public class OAuth2ServerConfig {
 			endpoints
 					.tokenStore(tokenStore())
 					.authenticationManager(authenticationManager)
-					.userDetailsService(userDetailsService);
+					.userDetailsService(userDetailsService)
+					.tokenServices(tokenServices());
+		}
+
+		@Primary
+		@Bean
+		public AuthorizationServerTokenServices tokenServices() {
+			DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+			defaultTokenServices.setAccessTokenValiditySeconds(-1);
+			defaultTokenServices.setRefreshTokenValiditySeconds(-1);
+			defaultTokenServices.setSupportRefreshToken(true);
+			defaultTokenServices.setReuseRefreshToken(false);
+			defaultTokenServices.setTokenStore(tokenStore());
+			return defaultTokenServices;
 		}
 	}
 
