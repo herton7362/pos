bb365a2107d3d917df85c6d73fa921c24a57784c
zdf <zdf@PC-20151012IXTG>
2018-09-12 17:10:53 +0800

    测试

--- src/main/java/com/framework/module/sn/web/SnController.java
+++ src/main/java/com/framework/module/sn/web/SnController.java
@@ -1,21 +1,23 @@
 package com.framework.module.sn.web;
 
-import com.framework.module.member.domain.AchievementDetail;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.framework.module.sn.domain.SnInfo;
 import com.framework.module.sn.service.SnInfoService;
 import com.kratos.common.AbstractCrudController;
-import com.kratos.exceptions.BusinessException;
-import com.kratos.module.auth.UserThread;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
-@Api(value = "SN管理11")
+@Api(value = "SN管理1111")
 @RestController
 @RequestMapping("/api/sn")
 public class SnController extends AbstractCrudController<SnInfo> {
