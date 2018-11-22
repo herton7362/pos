371ae161278471342387fa75afa8d6ab325e02ef
zhangqinghe123 <qingheemail@126.com>
2018-11-22 13:50:30 +0800

    增加确认功能

--- src/main/resources/static/js/modules/memberCashInAudit/list.js
+++ src/main/resources/static/js/modules/memberCashInAudit/list.js
@@ -1,49 +1,55 @@
-require(['jquery', 'vue', 'messager', 'utils'], function($, Vue, messager, utils) {
+require(['jquery', 'vue', 'messager', 'utils'], function ($, Vue, messager, utils) {
     new Vue({
         el: '#content',
         data: {
             crudgrid: {
                 $instance: {},
                 queryParams: {
-                	collectName: ''
+                    collectName: ''
                 },
                 columns: [
-                    {field:'member.name', title:'会员'},
-                    {field:'cashAmount', title:'提现金额'},
-                    {field:'collectAccount', title:'收款银行卡卡号'},
-                    {field:'collectName', title:'收款人'},
-                    {field:'serialNum', title:'流水单号'},
-                    {field:'bankName', title:'银行卡银行'},
-                    {field:'status', title:'认证状态' , formatter: function(value) {
-                        if('PENDING' === value) {
-                            return '待审核';
-                        } else if('PASS' === value) {
-                            return '已通过';
-                        } else if('UN_PASS' === value) {
-                            return '未通过';
+                    {field: 'member.name', title: '会员'},
+                    {field: 'cashAmount', title: '提现金额'},
+                    {field: 'collectAccount', title: '收款银行卡卡号'},
+                    {field: 'collectName', title: '收款人'},
+                    {field: 'serialNum', title: '流水单号'},
+                    {field: 'bankName', title: '银行卡银行'},
+                    {
+                        field: 'status', title: '认证状态', formatter: function (value) {
+                            if ('PENDING' === value) {
+                                return '待审核';
+                            } else if ('PASS' === value) {
+                                return '已通过';
+                            } else if ('UN_PASS' === value) {
+                                return '未通过';
+                            }
                         }
-                        }},
-                    {field:'createdDate', title:'申请时间', formatter: function (value) {
-                        return new Date(value).format('yyyy-MM-dd HH:mm')
-                    }},
-                    {field:'reason', title:'提现失败理由'},
-                    {field:'payOrderState', title:'转账状态' , formatter: function(value) {
-                            if('UN_PAY' === value) {
+                    },
+                    {
+                        field: 'createdDate', title: '申请时间', formatter: function (value) {
+                            return new Date(value).format('yyyy-MM-dd HH:mm')
+                        }
+                    },
+                    {field: 'reason', title: '提现失败理由'},
+                    {
+                        field: 'payOrderState', title: '转账状态', formatter: function (value) {
+                            if ('UN_PAY' === value) {
                                 return '未转账';
-                            } else if('SUCCESS' === value) {
+                            } else if ('SUCCESS' === value) {
                                 return '成功';
-                            } else if('IN_PROCESS' === value) {
+                            } else if ('IN_PROCESS' === value) {
                                 return '处理中';
-                            } else if('FAIL' === value) {
+                            } else if ('FAIL' === value) {
                                 return '失败';
-                            } else if('CREATE' === value) {
+                            } else if ('CREATE' === value) {
                                 return '已受理';
-                            } else if('REFUSE' === value) {
+                            } else if ('REFUSE' === value) {
                                 return '拒绝交易';
                             }
-                        }},
-                    {field:'payResultCode', title:'支付机构返回码'},
-                    {field:'payResultDes', title:'支付机构返回描述'}
+                        }
+                    },
+                    {field: 'payResultCode', title: '支付机构返回码'},
+                    {field: 'payResultDes', title: '支付机构返回描述'}
                 ]
             },
             formData: {
@@ -68,7 +74,7 @@ require(['jquery', 'vue', 'messager', 'utils'], function($, Vue, messager, utils
                 var self = this;
                 $.each(data, function (k, row) {
                     $.each(self.member.data, function () {
-                        if(row.memberId === this.id)
+                        if (row.memberId === this.id)
                             row.member = this;
                     })
                 });
@@ -76,6 +82,24 @@ require(['jquery', 'vue', 'messager', 'utils'], function($, Vue, messager, utils
             },
             choseFile: function () {
                 window.location.href = utils.patchUrlPrefixUrl('/memberprofit/exportCashInRecords')
+            },
+            pay: function (row) {
+                messager.alert('确认给该用户转账？', function () {
+                    var self = this;
+                    $.ajax({
+                        url: utils.patchUrl('/api/payHistory/pay'),
+                        data: {
+                            cashInId: row.id
+                        },
+                        type: 'GET',
+                        success: function () {
+                            messager.bubble("转账成功");
+                            self.crudgrid.$instance.load({
+                                status: 'PASS'
+                            });
+                        }
+                    })
+                });
             }
         },
         watch: {
@@ -85,7 +109,7 @@ require(['jquery', 'vue', 'messager', 'utils'], function($, Vue, messager, utils
                 });
             }
         },
-        mounted: function() {
+        mounted: function () {
             var self = this;
             $.ajax({
                 url: utils.patchUrl('/api/member'),
@@ -94,9 +118,9 @@ require(['jquery', 'vue', 'messager', 'utils'], function($, Vue, messager, utils
                     order: 'asc,desc',
                     logicallyDeleted: false
                 },
-                success: function(data) {
+                success: function (data) {
                     $.each(data.content, function () {
-                        if(!this.name) {
+                        if (!this.name) {
                             this.name = this.loginName;
                         }
                     });
